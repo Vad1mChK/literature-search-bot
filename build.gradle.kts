5768e4b05e7a48ba8b80ddd2e1226590063b8ddd
Vadim Chaykin <snusmumrike2@gmail.com>
2026-02-18 17:52:09 +0300

    added CI and tests

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "com.vad1mchk.litsearchbot"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/ExampleTest.kt
@@ -0,0 +1,12 @@
+package com.vad1mchk.litsearchbot
+
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+class ExampleTest {
+    @Test
+    fun simpleTest() {
+        assertNotEquals(0.1 + 0.2, 0.3)
+    }
+}
