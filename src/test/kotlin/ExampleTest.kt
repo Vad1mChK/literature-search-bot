838eebd13c5e7626c12ee8368b49df7e67be1938
Vadim Chaykin <snusmumrike2@gmail.com>
2026-02-21 13:35:14 +0300

    fixed ktlint

--- src/test/kotlin/ExampleTest.kt
+++ src/test/kotlin/ExampleTest.kt
@@ -1,7 +1,6 @@
 package com.vad1mchk.litsearchbot
 
 import org.junit.jupiter.api.Assertions.assertNotEquals
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 class ExampleTest {
